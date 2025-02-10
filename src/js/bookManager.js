991d4481c086cbfe0ebce7a5d6e9fe7d4d763381
chalita <chalita.antonsen@gmail.com>
2025-02-10 09:45:46 +0100

    store books collection

--- src/js/bookManager.js
+++ src/js/bookManager.js
@@ -2,7 +2,7 @@ import AudioBook from "./audioBook";
 import PrintedBook from "./printedBooks";
 
 class BookManager {
-  // storing the books
+  // STORING BOOKS
   static booksCollection =
     JSON.parse(localStorage.getItem("book-collection")) || []; //if it is empty when you first open the page  add this:|| [] as an empty array, if not it will show the existing data
   static addBook(
@@ -41,7 +41,15 @@ class BookManager {
     // adding books to the collection
     // this keyword points to the class itself (BookManager) or if it was not static it will point to the instance that was created
     this.booksCollection.push(book);
+    // add the method for storing the books here
+    this.storeBooks(this.booksCollection);
     console.log(this.booksCollection);
+  }
+  // USING A METHOD FOR STORING THE BOOKS
+  //has to be string or JSON
+  //responsible for updating the local storage
+  static storeBooks(collection) {
+    localStorage.setItem("books-collection", JSON.stringify(collection));
   }
 }
 
